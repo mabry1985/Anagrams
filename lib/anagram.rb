7cbc840a3ea132d82fd95acb2887240820acda8e
mabry1985 <mabry1985@gmail.com>
2019-07-19 10:11:43 -0700

    write first spec and begin logic to compare whether or not two words are anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/ruby
   require 'pry'
-binding.pry
+  require 'ruby-dictionary'
+  dictionary = Dictionary.from_file('./lib/dictionary.txt')
+
+class AnagramFinder
+  attr_accessor(:word)
+
+  def initialize(word)
+      @word = word
+  end
+
+  def anagram(other_word)
+    if @word.length === other_word.length
+      
+
+    else
+      "These words are not anagrams of each other"
+    end
+  end
+end
