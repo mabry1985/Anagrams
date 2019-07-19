807943d660d8448d5b4878c9dd51bae6fc7e015d
mabry1985 <mabry1985@gmail.com>
2019-07-19 12:20:48 -0700

    write second spec for anagram method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,19 +3,44 @@
   require 'ruby-dictionary'
   dictionary = Dictionary.from_file('./lib/dictionary.txt')
 
-class AnagramFinder
-  attr_accessor(:word)
+class Word
+  attr_accessor(:word, :anagram)
 
   def initialize(word)
       @word = word
+      @anagram = true
   end
 
-  def anagram(other_word)
+  def anagram?(other_word)
+    word_array = @word.split('')
+    other_word_array = other_word.split('')
     if @word.length === other_word.length
-      
-
+      x = 0
+      while (x < word_array.length)
+        other_word_array.delete_if do |letter|
+          if ((word_array[x] === letter) & (other_word_array.length > 0))
+            @anagram = true
+            x += 1
+          elsif ((word_array[x] != letter))
+            @anagram = false
+          else
+            x += 1
+          end
+        end
+      end
     else
-      "These words are not anagrams of each other"
+      @anagram = false
     end
+    @anagram
   end
+
+
 end
+
+# puts "Enter two words to see if they are anagrams"
+# puts "Enter first word"
+# first_word = gets.chomp
+# puts "Enter second word"
+# second_word = gets.chomp
+# word_object = Word.new(first_word)
+# word_object.anagram?(second_word)
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,6 +1,12 @@
 require 'rspec'
 require 'anagram'
 
-describe('anagram') do
-expect(is_anagram?('ripped', 'dipper')).to(eq(true))
+describe('#anagram finder') do
+  ripped = Word.new('ripped')
+  it ('should return true if the words are anagrams') do
+  expect(ripped.anagram?('dipper')).to(eq(true))
+  end
+  it ('should return false if the words are not anagrams') do
+  expect(ripped.anagram?('horse')).to(eq(false))
+  end
 end
